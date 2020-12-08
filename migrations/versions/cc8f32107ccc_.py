6a48b35439489a9e68c2a07436c30bf942a00f68
paachary <prashant_acharya74@yahoo.com>
2020-12-08 17:58:46 +0530

    adding the migrations folder

---
+++ migrations/versions/cc8f32107ccc_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: cc8f32107ccc
+Revises: 
+Create Date: 2020-12-06 22:02:02.800955
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cc8f32107ccc'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
